2da7505d5de356af362d699be056732215964474
AndreaMedinaRico <a01705541@tec.mx>
2026-04-21 17:00:16 -0600

    chore: create model and separate in folder

--- prediction.py
+++ prediction.py
@@ -13,31 +13,31 @@ import joblib
 from Transformation import Transformation
 
 # Initial imports
-model = keras.models.load_model('model_baseline.keras')
-scaler = joblib.load('scaler.joblib')
+model = keras.models.load_model('models/model_baseline.keras')
+scaler = joblib.load('models/scaler.pkl')
 
 def get_user_input():
     user_data = {}
 
-    print("Hi! Welcome to the stress level predictor. Please provide the following information:")
+    print("\n\nHi! Welcome to the stress level predictor. Please provide the following information:\n")
 
-    user_data["age"] = int(input("Age: "))
-    user_data["gender"] = int(input("Assigned sex at birth:\n 0: Male\n 1: Female"))
-    user_data["occupation"] = int(input("Occupation:\n 1: Designer\n 2: Teacher\n 3: Software Engineer\n 4: Manager\n" \
+    user_data["age"] = int(input("\nAge: "))
+    user_data["gender"] = int(input("\nAssigned sex at birth:\n 0: Male\n 1: Female\n"))
+    user_data["occupation"] = int(input("\nOccupation:\n 1: Designer\n 2: Teacher\n 3: Software Engineer\n 4: Manager\n" \
                                       "  5: Student\n 6: Freelancer\n 7: Doctor\n 8: Researcher\n"))
     
-    user_data["daily_screen_time_hours"] = float(input("Daily screen time (hours): "))
-    user_data["phone_usage_before_sleep_minutes"] = float(input("Phone usage before sleep (minutes): "))
+    user_data["daily_screen_time_hours"] = float(input("\nDaily screen time (hours): "))
+    user_data["phone_usage_before_sleep_minutes"] = float(input("\nPhone usage before sleep (minutes): "))
     
-    user_data["sleep_duration_hours"] = float(input("Sleep duration (hours): "))
-    user_data["sleep_quality_score"] = float(input("Sleep quality (0-10): "))
+    user_data["sleep_duration_hours"] = float(input("\nSleep duration (hours): "))
+    user_data["sleep_quality_score"] = float(input("\nSleep quality (0-10): "))
     
-    user_data["caffeine_intake_cups"] = float(input("Caffeine intake (cups per day): "))
+    user_data["caffeine_intake_cups"] = float(input("\nCaffeine intake (cups per day): "))
     
-    user_data["physical_activity_minutes"] = float(input("Physical activity (minutes per day): "))
-    user_data["notifications_received_per_day"] = int(input("Notifications received per day: "))
+    user_data["physical_activity_minutes"] = float(input("\nPhysical activity (minutes per day): "))
+    user_data["notifications_received_per_day"] = int(input("\nNotifications received per day: "))
     
-    user_data["mental_fatigue_score"] = float(input("Mental fatigue (0-10): "))
+    user_data["mental_fatigue_score"] = float(input("\nMental fatigue (0-10): "))
 
     return user_data
 
@@ -71,3 +71,12 @@ def preprocess_input(user_data):
 
     final_df = trans.data
     return final_df
+
+
+user_data = get_user_input()
+preprocessed_data = preprocess_input(user_data)
+
+prediction = model.predict(preprocessed_data)
+
+stress_level = np.argmax(prediction)
+print(f"\nPredicted stress level: {stress_level:.2f}")
